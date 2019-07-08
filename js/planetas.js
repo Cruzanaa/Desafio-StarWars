8c33a7fffb5150339a19dd71eae1fa9459e74957
Ana Carolina Ferreira <cruzana.design@gmail.com>
2019-07-08 15:03:10 -0300

    Commit: criando javascrip, primeira parte

--- js/planetas.js
+++ js/planetas.js
@@ -1,9 +1,24 @@
-var planetas =[{
-    nome : 'hutta',
-    terreno: '',
-    filmes:'',
+var planetas = [
+    {
+    nome: 'hoth',
+    clima: 'frio',
+    terreno:'arrido',
+    filmes: '2 filmes',
+},{
+    nome: 'taris',
+    clima: 'quente',
+    filmes: '1 filme',
+},{
+    nome: 'hutta',
+    clima: 'deserto',
+    filmes: '1 filme',
 }]
 
-var botaoRandom = document.querySelector('#RandomPlaneta');
+var botaoRandom = document.querySelector('#BotaoRandom');
 
-botaoRandom.addEventListener('click');
+botaoRandom.addEventListener('click', criarRandom);
+
+function criarRandom(evento){
+    document.getElementsByName('.planetas').innerHTML=Math.floor(Math.random()*20);
+    console.log(planetas);
+}
