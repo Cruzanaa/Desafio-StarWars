1d5ce839232b8cf113b6075b6adabd09e5eb1b8d
Cruzanaa <cruzana.design@gmail.com>
2019-07-09 21:59:21 -0300

    Commit: nova pagina; novo javascript

--- js/planetas.js
+++ js/planetas.js
@@ -1,24 +1,88 @@
-var planetas = [
-    {
-    nome: 'hoth',
-    clima: 'frio',
-    terreno:'arrido',
-    filmes: '2 filmes',
+var Planetas = document.querySelectorAll('.planetas');
+
+Planetas = [{
+    nome: 'alderaan',
+    population: '20000000000',
+    clima: 'temperate',
+    terreno: 'grasslands / mountains', 
+    filmes: '6 e 1',
 },{
-    nome: 'taris',
-    clima: 'quente',
-    filmes: '1 filme',
+    nome: 'Yavin IV',
+    population: '1000',
+    clima: 'temperate / tropical',
+    terreno: 'grasslands / mountains', 
+    filmes: '6 e 1', 
 },{
-    nome: 'hutta',
-    clima: 'deserto',
-    filmes: '1 filme',
+    nome: 'Hoth',
+    population: 'unknown',
+    clima: 'frozen',
+    terreno: 'tundra/ ice caves / mountain ranges', 
+    filmes: '2',
+},{
+    nome: 'Dagobah',
+    population: 'unknown',
+    clima: 'murky',
+    terreno: 'swamp / jungles', 
+    filmes: '2 / 6 / 3',
+},{
+    nome: 'Bespin',
+    population: '118000',
+    clima: 'temperate',
+    terreno: 'gas giant', 
+    filmes: '2',
+},{
+    nome: 'Endor',
+    population: '3000000',
+    clima: 'temperate',
+    terreno: 'florest / mountains / lakes', 
+    filmes: '3',
+},{
+    nome: 'Naboo',
+    population: '450000000',
+    clima: 'temperate',
+    terreno: 'glassy hills / swamps / florests / mountains', 
+    filmes: '5 / 4 / 6 / 3',
+},{ 
+    nome: 'Coruscant',
+    population: '1000000000000',
+    clima: 'temperate',
+    terreno: 'cityscape / mountains', 
+    filmes: '5 / 4 / 6 / 3',
+},{
+    nome: 'Kamino',
+    population: '10000000',
+    clima: 'temperate',
+    terreno: 'ocean', 
+    filmes: '5',
+},{
+    nome: 'Mustafar',
+    population: '20000',
+    clima: 'hot',
+    terreno: 'volcanoes / lava rivers / mountains / caves', 
+    filmes: '6',
+},{
+    nome: 'Utapau',
+    population: '95000000',
+    clima: 'temperate / arid / windy',
+    terreno: 'scrublands / savanna / canyons / sinkholes', 
+    filmes: '6',
 }]
+
 
 var botaoRandom = document.querySelector('#BotaoRandom');
 
 botaoRandom.addEventListener('click', criarRandom);
 
+
 function criarRandom(evento){
-    document.getElementsByName('.planetas').innerHTML=Math.floor(Math.random()*20);
-    console.log(planetas);
+    let Aletorio;
+  for( var SorteioPlanetas = 0; SorteioPlanetas > Planetas.length; SorteioPlanetas ++){
+  
+  }
 }
+
+/*var random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+  document.getElementsByName('.planetas').innerHTML=Math.floor(Math.random()*20);
+    console.log(planetas);*/
