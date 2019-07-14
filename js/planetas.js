388b3e9b1759b7dff95fcb84494334ea2588ac63
Cruzanaa <cruzana.design@gmail.com>
2019-07-14 11:37:27 -0300

    Commit: Sorteio Planeta Finalizado

--- js/planetas.js
+++ js/planetas.js
@@ -1,13 +1,13 @@
-var Planetas = document.querySelectorAll('.planetas');
+//var Planetas = document.querySelectorAll('.planetas');
 
-Planetas = [{
-    nome: 'Alderaan',
+var Planetas = [{
+    nome: 'Planeta: ALDERAAN',
     id: '1',
-    populacao: '20000000000',
-    clima: 'temperate',
-    terreno: 'grasslands / mountains', 
-    filmes: '6 e 1',
-    imagem: 'url(img/yavin.png)'
+    populacao: 'População: 20000000000',
+    clima: ' Clima: temperado',
+    terreno: 'Terreno: Pastagem / Montanhas', 
+    filmes: 'Filmes: 6 e 1',
+    imagem: 'url(img/alderaan.png)'
 },{
     nome: 'Yavin IV',
     id: '2',
@@ -90,6 +90,7 @@ Planetas = [{
     imagem: 'url(img/naboo.png)'
 }]
 
+var planetaAleatorio;
 
 var botaoRandom = document.querySelector('#BotaoRandom');
 
@@ -97,9 +98,9 @@ botaoRandom.addEventListener('click', criarRandom);
 
 
 function criarRandom(){
-    aleatorio= Math.floor(Math.random()*11);  /*--criando aleatório--*/
+    aleatorio = Math.floor(Math.random() * 11);  /*--criando aleatório--*/
     planetaAleatorio = Planetas[aleatorio];
-    alert(planetaAleatorio['nome']);
+    mostrarInfo();
 }
 
 
@@ -108,35 +109,16 @@ var botaoInfo = document.querySelector('.infoPlanetas');
 botaoInfo.addEventListener('click', mostrarInfo);
 
   function mostrarInfo(){
-    document.getElementById('nome').innerHTML = Planetas.find[11],[nome];
-    document.getElementById('clima').innerHTML = Planetas.find[clima];
-    document.getElementById('terreno').innerHTML = Planetas.find[terreno];
-    document.getElementById('filmes').innerHTML = Planetas.find[filmes];
-}
-
- /*function inforPlanetas(){
-    var PlanetasInfo = document.querySelector('.InfoPlanetas');
-        PlanetasInfo = document.createElement('p');
-        PlanetasInfo.classList.add('texto-info');
-        
-    }*/
-
+   document.getElementById('nome').innerHTML = planetaAleatorio.nome;
+      document.getElementById('clima').innerHTML = planetaAleatorio.clima;
+      document.getElementById('terreno').innerHTML = planetaAleatorio.terreno;
+      document.getElementById('filmes').innerHTML = planetaAleatorio.filmes;
+      //document.getElementById('imgPlaneta').innerHTML = planetaAleatorio.imagem;
+      document.getElementById('imgPlaneta').style.backgroundImage = planetaAleatorio.imagem;
+    
+    }
 
         function printPlanet () {
-            // fetch('https://swapi.co/api/planets/?page=1&format=json')
-            //     .then(function (resposta) {
-            //         return resposta.json();
-            //     })
-            //     .then(function (planetas) {
-            //         planetas.results.forEach(planeta => {
-            //             var planetaDiv = document.querySelector('.planetas');
-            //             planetaDiv.appendChild(montaPlaneta(planeta.name));
-            //         });
-            //     })
-            //     .catch(function (err) {
-            //         console.log("Something went wrong!", err);
-            //     });
-            
             Planetas.forEach(planeta => {
                         var planetaDiv = document.querySelector('.planetas');
                         planetaDiv.appendChild(montaPlaneta(planeta.name, planeta.imagem));
